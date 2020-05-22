478d6b3b7867c691c3d1329edde808081162644e
bjoern <git@wotanii.de>
2020-05-22 16:07:55 +0200

    enforcing valid attibutes for all configuration classes

--- neuro_evolution_ctrnn/tools/configurations.py
+++ neuro_evolution_ctrnn/tools/configurations.py
@@ -1,17 +1,27 @@
 # the configurations need to be in a separate file from the actual objects to avoid circular imports
-from typing import Iterable
-from collections import namedtuple
+from typing import Iterable, List
 
 
-class EpisodeRunnerCfg:
+class ConfigBase:
+    # slots saves a little bit of memory, but more importantly
+    # it makes sure no unexpected attributes can appear on the object, which prevents typos among other things
+    __slots__: List[str] = []
+
+    def __init__(self, **attr):
+        for key in attr:
+            setattr(self, key, attr[key])
+        for key in dir(self):
+            if not hasattr(self, key):
+                raise RuntimeError("key missing: " + key)
+
+
+class EpisodeRunnerCfg(ConfigBase):
+    __slots__ = ['number_fitness_runs', 'keep_env_seed_fixed_during_generation']
     number_fitness_runs: int
     keep_env_seed_fixed_during_generation: bool
 
-    def __init__(self, **attr):
-        self.__dict__.update(attr)
 
-
-class ContinuousTimeRNNCfg:
+class ContinuousTimeRNNCfg(ConfigBase):
     __slots__ = ['optimize_y0', 'delta_t', 'optimize_state_boundaries',
                  'set_principle_diagonal_elements_of_W_negative', 'number_neurons',
                  'normalize_input', 'clipping_range_min', 'clipping_range_max', 'normalize_input_target']
@@ -22,25 +32,20 @@ class ContinuousTimeRNNCfg:
     number_neurons: int
     normalize_input: bool
     normalize_input_target: float
-    clipping_range_min: Iterable[float]
-    clipping_range_max: Iterable[float]
+    clipping_range_min: float
+    clipping_range_max: float
 
-    def __init__(self, **attr):
-        for key in attr:
-            setattr(self, key, attr[key])
-        for key in dir(self):
-            if not hasattr(self, key):
-                raise RuntimeError("key missing: " + key)
 
-class TrainerCmaEsCfg:
+class TrainerCmaEsCfg(ConfigBase):
+    __slots__ = ['population_size', 'sigma']
     population_size: int
     sigma: float
 
-    def __init__(self, **attr):
-        self.__dict__ = attr
 
+class ExperimentCfg(ConfigBase):
+    __slots__ = ['neural_network_type', 'environment', 'random_seed',
+                 'trainer_type', 'number_generations', 'brain', 'episode_runner', 'trainer', 'raw_dict']
 
-class ExperimentCfg:
     neural_network_type: str
     environment: str
     random_seed: int
@@ -49,7 +54,4 @@ class ExperimentCfg:
     brain: ContinuousTimeRNNCfg
     episode_runner: EpisodeRunnerCfg
     trainer: TrainerCmaEsCfg
-    _raw_dict: dict
-
-    def __init__(self, **attr):
-        self.__dict__.update(attr)
+    raw_dict: dict
