50425f3b800cf78152fd4d5dad85293bab5f2a7e
bjoern <git@wotanii.de>
2020-05-22 21:23:33 +0200

    .

--- neuro_evolution_ctrnn/visualize.py
+++ neuro_evolution_ctrnn/visualize.py
@@ -17,7 +17,7 @@ def parse_args(args=None):
     parser = argparse.ArgumentParser(description='visualize CTRNN')
     parser.add_argument('--configuration', metavar='dir', type=str,
                         help='use an alternative configuration file',
-                        default=os.path.join('results', '2020-05-22_14-56-13'))
+                        default=os.path.join('results/data', '2020-05-22_16-38-09'))
 
     parser.add_argument('--plot', metavar='dir', type=bool,
                         help='show plot?',
