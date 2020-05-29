8f4f1c91710b1964ad28eb945bfa788e8ec4afa6
bjoern <git@wotanii.de>
2020-05-29 11:47:38 +0200

    simplified configurations by using python-attr. Note: Now the classes behave exactly as they did before

--- neuro_evolution_ctrnn/tools/configurations.py
+++ neuro_evolution_ctrnn/tools/configurations.py
@@ -2,65 +2,48 @@
 from typing import List
 import attr
 
-class ConfigBase:
-    # slots saves a little bit of memory, but more importantly
-    # it makes sure no unexpected attributes can appear on the object, which prevents typos among other things
-    __slots__: List[str] = []
 
-    def __init__(self, **attr):
-        for key in attr:
-            setattr(self, key, attr[key])
-        for key in dir(self):
-            if not hasattr(self, key):
-                # this branch is reached, when an attribute of the class hasn't been initialised from the dict
-                raise RuntimeError("key missing: " + key)
+@attr.s(slots=True)
+class EpisodeRunnerCfg:
+    number_fitness_runs: int = attr.ib()
+    keep_env_seed_fixed_during_generation: bool = attr.ib()
 
 
-class EpisodeRunnerCfg(ConfigBase):
-    __slots__ = ['number_fitness_runs', 'keep_env_seed_fixed_during_generation']
-    number_fitness_runs: int
-    keep_env_seed_fixed_during_generation: bool
+@attr.s(slots=True)
+class ContinuousTimeRNNCfg:
+    type: str = attr.ib()
+    optimize_y0: bool = attr.ib()
+    delta_t: float = attr.ib()
+    optimize_state_boundaries: str = attr.ib()
+    set_principle_diagonal_elements_of_W_negative: bool = attr.ib()
+    number_neurons: int = attr.ib()
+    normalize_input: bool = attr.ib()
+    normalize_input_target: float = attr.ib()
+    clipping_range_min: float = attr.ib()
+    clipping_range_max: float = attr.ib()
+    v_mask: str = attr.ib()
+    v_mask_param: float = attr.ib()
+    w_mask: str = attr.ib()
+    w_mask_param: float = attr.ib()
+    t_mask: str = attr.ib()
+    t_mask_param: float = attr.ib()
+    parameter_perturbations: float = attr.ib()
 
 
-class ContinuousTimeRNNCfg(ConfigBase):
-    __slots__ = ['type', 'optimize_y0', 'delta_t', 'optimize_state_boundaries',
-                 'set_principle_diagonal_elements_of_W_negative', 'number_neurons',
-                 'normalize_input', 'clipping_range_min', 'clipping_range_max', 'normalize_input_target', 'v_mask',
-                 'v_mask_param', 'w_mask', 'w_mask_param', 't_mask', 't_mask_param', 'parameter_perturbations']
-    type: str
-    optimize_y0: bool
-    delta_t: float
-    optimize_state_boundaries: str
-    set_principle_diagonal_elements_of_W_negative: bool
-    number_neurons: int
-    normalize_input: bool
-    normalize_input_target: float
-    clipping_range_min: float
-    clipping_range_max: float
-    v_mask: str
-    v_mask_param: float
-    w_mask: str
-    w_mask_param: float
-    t_mask: str
-    t_mask_param: float
-    parameter_perturbations: float
+@attr.s(slots=True)
+class OptimizerCmaEsCfg:
+    type: str = attr.ib()
+    population_size: int = attr.ib()
+    sigma: float = attr.ib()
+    checkpoint_frequency: int = attr.ib()
 
 
-class OptimizerCmaEsCfg(ConfigBase):
-    __slots__ = ['type', 'population_size', 'sigma', 'checkpoint_frequency']
-    type: str
-    population_size: int
-    sigma: float
-    checkpoint_frequency: int
-
-
-class ExperimentCfg(ConfigBase):
-    __slots__ = ['environment', 'random_seed', 'number_generations', 'brain', 'episode_runner', 'optimizer', 'raw_dict']
-
-    environment: str
-    random_seed: int
-    number_generations: int
-    brain: ContinuousTimeRNNCfg
-    episode_runner: EpisodeRunnerCfg
-    optimizer: OptimizerCmaEsCfg
-    raw_dict: dict
+@attr.s(slots=True)
+class ExperimentCfg:
+    environment: str = attr.ib()
+    random_seed: int = attr.ib()
+    number_generations: int = attr.ib()
+    brain: ContinuousTimeRNNCfg = attr.ib()
+    episode_runner: EpisodeRunnerCfg = attr.ib()
+    optimizer: OptimizerCmaEsCfg = attr.ib()
+    raw_dict: dict = attr.ib()
