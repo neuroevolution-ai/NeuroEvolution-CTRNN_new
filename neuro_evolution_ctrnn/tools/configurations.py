13f4cb898dcfd9158dd6d7ec3d38b5c7437c52b8
bjoern <git@wotanii.de>
2020-05-29 12:02:00 +0200

    simplified configurations by using python-attr. Note: Now the classes behave exactly as they did before

--- neuro_evolution_ctrnn/tools/configurations.py
+++ neuro_evolution_ctrnn/tools/configurations.py
@@ -3,47 +3,47 @@ from typing import List
 import attr
 
 
-@attr.s(slots=True)
+@attr.s(slots=True, auto_attribs=True)
 class EpisodeRunnerCfg:
-    number_fitness_runs: int = attr.ib()
-    keep_env_seed_fixed_during_generation: bool = attr.ib()
+    number_fitness_runs: int
+    keep_env_seed_fixed_during_generation: bool
 
 
-@attr.s(slots=True)
+@attr.s(slots=True, auto_attribs=True)
 class ContinuousTimeRNNCfg:
-    type: str = attr.ib()
-    optimize_y0: bool = attr.ib()
-    delta_t: float = attr.ib()
-    optimize_state_boundaries: str = attr.ib()
-    set_principle_diagonal_elements_of_W_negative: bool = attr.ib()
-    number_neurons: int = attr.ib()
-    normalize_input: bool = attr.ib()
-    normalize_input_target: float = attr.ib()
-    clipping_range_min: float = attr.ib()
-    clipping_range_max: float = attr.ib()
-    v_mask: str = attr.ib()
-    v_mask_param: float = attr.ib()
-    w_mask: str = attr.ib()
-    w_mask_param: float = attr.ib()
-    t_mask: str = attr.ib()
-    t_mask_param: float = attr.ib()
-    parameter_perturbations: float = attr.ib()
+    type: str
+    optimize_y0: bool
+    delta_t: float
+    optimize_state_boundaries: str
+    set_principle_diagonal_elements_of_W_negative: bool
+    number_neurons: int
+    normalize_input: bool
+    normalize_input_target: float
+    clipping_range_min: float
+    clipping_range_max: float
+    v_mask: str
+    v_mask_param: float
+    w_mask: str
+    w_mask_param: float
+    t_mask: str
+    t_mask_param: float
+    parameter_perturbations: float
 
 
-@attr.s(slots=True)
+@attr.s(slots=True, auto_attribs=True)
 class OptimizerCmaEsCfg:
-    type: str = attr.ib()
-    population_size: int = attr.ib()
-    sigma: float = attr.ib()
-    checkpoint_frequency: int = attr.ib()
+    type: str
+    population_size: int
+    sigma: float
+    checkpoint_frequency: int
 
 
-@attr.s(slots=True)
+@attr.s(slots=True, auto_attribs=True)
 class ExperimentCfg:
-    environment: str = attr.ib()
-    random_seed: int = attr.ib()
-    number_generations: int = attr.ib()
-    brain: ContinuousTimeRNNCfg = attr.ib()
-    episode_runner: EpisodeRunnerCfg = attr.ib()
-    optimizer: OptimizerCmaEsCfg = attr.ib()
-    raw_dict: dict = attr.ib()
+    environment: str
+    random_seed: int
+    number_generations: int
+    brain: ContinuousTimeRNNCfg
+    episode_runner: EpisodeRunnerCfg
+    optimizer: OptimizerCmaEsCfg
+    raw_dict: dict
