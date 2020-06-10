3c1334a76e21c425fdec50b1f9f3200e64615587
Patrick Deubel <patrick.deubel@gmail.com>
2020-06-10 11:10:34 +0200

    Fixed LSTM test to check if at least 98% of variables are close between PyTorch and NumPy implementations. Also added time comparison.

--- tests/test_lstm.py
+++ tests/test_lstm.py
@@ -5,6 +5,7 @@ from tools.configurations import ExperimentCfg, LSTMCfg
 import numpy as np
 import gym
 import torch
+import time
 
 
 class TestLSTM:
@@ -14,7 +15,7 @@ class TestLSTM:
         input_size = 28
         output_size = 8
 
-        number_of_inputs = 10
+        number_of_inputs = 10000
 
         input_space = gym.spaces.Box(-1, 1, (input_size,))
         output_space = gym.spaces.Box(-1, 1, (output_size,))
@@ -25,7 +26,8 @@ class TestLSTM:
         individual_pytorch = np.random.randn(individual_size).astype(np.float32)
         individual_numpy = np.copy(individual_pytorch)
 
-        lstm_pytorch = LSTMPyTorch(input_space, output_space, individual_pytorch, lstm_config)
+        with torch.no_grad():
+            lstm_pytorch = LSTMPyTorch(input_space, output_space, individual_pytorch, lstm_config)
 
         lstm_numpy = LSTMNumPy(input_space, output_space, individual_numpy, lstm_config)
 
@@ -52,18 +54,47 @@ class TestLSTM:
         lstm_pytorch_outputs = []
         lstm_numpy_outputs = []
 
-        for i in inputs:
-            lstm_pytorch_output = lstm_pytorch.step(i)
-            lstm_pytorch_outputs.append(lstm_pytorch_output)
+        lstm_pytorch_times = []
+        lstm_numpy_times = []
 
+        for i in inputs:
+            with torch.no_grad():
+                time_s = time.time()
+                lstm_pytorch_output = lstm_pytorch.step(i)
+                lstm_pytorch_times.append(time.time() - time_s)
+                lstm_pytorch_outputs.append(lstm_pytorch_output)
+
+            time_s = time.time()
             lstm_numpy_output = lstm_numpy.step(i)
+            lstm_numpy_times.append(time.time() - time_s)
             lstm_numpy_outputs.append(lstm_numpy_output)
 
         lstm_pytorch_outputs = np.array(lstm_pytorch_outputs)
         lstm_numpy_outputs = np.array(lstm_numpy_outputs)
 
+        lstm_pytorch_times = np.array(lstm_pytorch_times)
+        lstm_numpy_times = np.array(lstm_numpy_times)
+
+        assert len(lstm_pytorch_outputs) == len(lstm_numpy_outputs)
+        assert lstm_pytorch_outputs.size == lstm_numpy_outputs.size
+
         print("PyTorch: ", lstm_pytorch_outputs)
         print("------------------------------------")
         print("NumPy: ", lstm_numpy_outputs)
 
-        assert np.allclose(lstm_pytorch_outputs, lstm_numpy_outputs)
+        print("PyTorch Mean Prediction Time {}s | NumPy Mean Prediction Time {}s"
+              .format(np.mean(lstm_pytorch_times), np.mean(lstm_numpy_times)))
+
+        print("PyTorch Stddev Prediction Time {}s | NumPy Stddev Prediction Time {}s"
+              .format(np.std(lstm_pytorch_times), np.std(lstm_numpy_times)))
+
+        print("PyTorch Max Prediction Time {}s | NumPy Max Prediction Time {}s"
+              .format(np.max(lstm_pytorch_times), np.max(lstm_numpy_times)))
+
+        print("PyTorch Min Prediction Time {}s | NumPy Min Prediction Time {}s"
+              .format(np.min(lstm_pytorch_times), np.min(lstm_numpy_times)))
+
+        close_percentage = np.count_nonzero(
+            np.isclose(lstm_pytorch_outputs, lstm_numpy_outputs)) / lstm_pytorch_outputs.size
+
+        assert close_percentage > 0.98
