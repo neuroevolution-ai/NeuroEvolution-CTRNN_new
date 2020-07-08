85f179d31f1e4acd446fbe586e26cfd1ad876ec0
Patrick Deubel <patrick.deubel@gmail.com>
2020-07-08 14:02:08 +0200

    Added custom environment derived from Reacher-v2: ReacherMemory-v0. The Reacher memory experiment refactored as a custom env.

---
+++ gym_memory_environments/__init__.py
@@ -0,0 +1,7 @@
+from gym.envs.registration import register
+
+register(
+    id="ReacherMemory-v0",
+    entry_point="gym_memory_environments.envs:ReacherMemoryEnv",
+    max_episode_steps=90  # Arbitrarily chosen, can be overwritten in the ReacherMemoryEnv class
+)
---
+++ gym_memory_environments/envs/reacher_memory_env.py
@@ -0,0 +1,40 @@
+import numpy as np
+from gym.envs.mujoco import ReacherEnv
+
+
+class ReacherMemoryEnv(ReacherEnv):
+
+    def __init__(self, observation_frames, memory_frames, action_frames, observation_mask):
+        super().__init__()
+
+        assert observation_frames > 0 and memory_frames > 0 and action_frames > 0
+
+        self.observation_frames = observation_frames
+        self.memory_frames = memory_frames
+        self.action_frames = action_frames
+        self.observation_mask = observation_mask
+
+        self.t = 0
+
+    def step(self, action):
+
+        if self.t <= self.observation_frames + self.memory_frames:
+            action = np.zeros(action.shape)
+
+        ob, rew, done, info = super().step(action)
+
+        if self.t >= self.observation_frames:
+            for index in self.observation_mask:
+                ob[index] = 0.0
+
+        if self.t < self.observation_frames + self.memory_frames:
+            rew = 0.0
+
+        self.t += 1
+
+        return ob, rew, done, info
+
+    def reset(self):
+        super().reset()
+        self.spec.max_episode_steps = self.observation_frames + self.memory_frames + self.action_frames
+        self.t = 0
