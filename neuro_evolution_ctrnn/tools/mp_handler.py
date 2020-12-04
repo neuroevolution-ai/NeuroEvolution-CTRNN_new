a0ed142a292fbb35055649b503ce71364b16ed61
Patrick Deubel <patrick.deubel@gmail.com>
2020-12-04 14:25:58 +0100

    MPHandler: Every call of map now creates a temporary multiprocessing.Pool for the calculation.
    A pool as an instance variable did not work with pytest for some reason and to be safe I chose to use a new pool every time map is called.

--- neuro_evolution_ctrnn/tools/mp_handler.py
+++ neuro_evolution_ctrnn/tools/mp_handler.py
@@ -4,11 +4,13 @@ from multiprocessing import Pool
 class MPHandler:
 
     def __init__(self, number_of_workers):
-        self.pool = Pool(processes=number_of_workers)
+        self.number_of_workers = number_of_workers
 
     def map(self, func, *iterable):
-        return self.pool.starmap(func, zip(*iterable))
+        with Pool(self.number_of_workers) as pool:
+            result = pool.starmap(func, zip(*iterable))
+        return result
 
     def cleanup(self):
-        self.pool.close()
-        self.pool.join()
+        # Pool cleans itself up
+        pass
