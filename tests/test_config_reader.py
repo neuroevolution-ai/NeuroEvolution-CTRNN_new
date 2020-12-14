566749001f90b28e82fa8287b777c41207ec5a0b
bjoern <git@wotanii.de>
2020-12-14 10:25:51 +0100

    cleanup

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -1,4 +1,5 @@
 from tools.config_reader import ConfigReader
+from tools.experiment import Experiment
 import os
 
 
@@ -9,3 +10,14 @@ class TestConfigReader:
         config_location = os.path.join(current_directory, "basic_test_config.json")
         config = ConfigReader.config_from_file(config_location)
         assert config.brain.number_neurons == 2
+
+    def test_cnn_init_exp(self, tmpdir):
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        config_location = os.path.join(current_directory, "../configurations/cnn_ctrnn.json")
+
+        config = ConfigReader.config_from_file(config_location)
+        Experiment(configuration=config,
+                   result_path=tmpdir,
+                   from_checkpoint=None,
+                   processing_framework="dask")
+
