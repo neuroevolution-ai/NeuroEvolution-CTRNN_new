12b21017d7778c0cb889e1e31eb887bc88c3d797
Björn Jürgens <4362465+bjuergens@users.noreply.github.com>
2020-12-16 15:06:33 +0100

    Update tests/test_config_reader.py
    Co-authored-by: Patrick Deubel <33164686+pdeubel@users.noreply.github.com>

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -6,8 +6,7 @@ import os
 class TestConfigReader:
 
     def test_basic_init(self):
-        current_directory = os.path.dirname(os.path.realpath(__file__))
-        config_location = os.path.join(current_directory, "basic_test_config.json")
+        config_location = os.path.join(os.getcwd(), "basic_test_config.json")
         config = ConfigReader.config_from_file(config_location)
         assert config.brain.number_neurons == 2
 
