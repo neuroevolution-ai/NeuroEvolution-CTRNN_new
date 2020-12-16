c778ec9cfd27a2b46cc905906f6cfec82074d2a0
Björn Jürgens <4362465+bjuergens@users.noreply.github.com>
2020-12-16 15:06:06 +0100

    Update tests/test_config_reader.py
    Co-authored-by: Patrick Deubel <33164686+pdeubel@users.noreply.github.com>

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -12,12 +12,10 @@ class TestConfigReader:
         assert config.brain.number_neurons == 2
 
     def test_cnn_init_exp(self, tmpdir):
-        current_directory = os.path.dirname(os.path.realpath(__file__))
-        config_location = os.path.join(current_directory, "../configurations/cnn_ctrnn.json")
+        config_location = os.path.join(os.getcwd(), "../configurations/cnn_ctrnn.json")
 
         config = ConfigReader.config_from_file(config_location)
         Experiment(configuration=config,
                    result_path=tmpdir,
                    from_checkpoint=None,
                    processing_framework="dask")
-
