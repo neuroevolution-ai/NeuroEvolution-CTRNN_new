eb01fc85581062725f0abfad57b5ca9324d98bd6
bjoern <git@wotanii.de>
2021-02-01 13:40:16 +0100

    fixed unit test

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -5,15 +5,11 @@ import os
 
 class TestConfigReader:
 
-    def test_basic_init(self):
+    def test_basic_init(self, tmpdir):
         config_location = os.path.join(os.getcwd(), "tests/basic_test_config.json")
         config = ConfigReader.config_from_file(config_location)
+
         assert config.brain.number_neurons == 2
-
-    def test_cnn_init_exp(self, tmpdir):
-        config_location = os.path.join(os.getcwd(), "configurations/cnn_ctrnn.json")
-
-        config = ConfigReader.config_from_file(config_location)
         Experiment(configuration=config,
                    result_path=tmpdir,
                    from_checkpoint=None,
